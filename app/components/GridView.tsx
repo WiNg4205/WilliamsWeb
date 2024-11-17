d1ba4acce7afb58b078d538b2750d4cfe912e6eb
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-11-17 19:04:38 +1100

    added MazeAI + fixed grid text in dark mode

--- app/components/GridView.tsx
+++ app/components/GridView.tsx
@@ -9,7 +9,7 @@ export default function GridView({ projects }: Props) {
   return (
     <div className="self-center grid grid-cols-4 bg-gray-500 gap-4 p-4">
       {Object.entries(projects).map(([key, project]) => (
-        <div className="p-4 bg-gray-200" key={key}>
+        <div className="p-4 bg-gray-200 text-black" key={key}>
             <Image src={`/${key}.png`} alt={`Project #${key} preview`} height={800} width={1600} className="p-4" priority />
             <h1 className="font-bold">{project.title}</h1>
             <p>{project.description}</p>          
